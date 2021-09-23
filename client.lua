d76ff70553d44e48403846cd7477e0e0e312bf15
Walien_ <43910644+Walien-dev@users.noreply.github.com>
2021-09-23 04:43:19 +0200

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,7 +1,7 @@
 Citizen.CreateThread(function()
 	while true do
 		Citizen.Wait(5) 
-		if not IsPedArmed(PlayerPedId(), 1) then
+		if (not IsPedArmed(PlayerPedId(), 1)) and (GetSelectedPedWeapon(PlayerPedId()) ~= GetHashKey('weapon_unarmed')) then
 	
 			DisableControlAction(0, 140, true) 
 			DisableControlAction(0, 141, true) 
